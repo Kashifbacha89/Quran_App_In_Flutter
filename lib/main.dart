43c3cc060824c5f435ef886360f11825edff0637
unknown <muhammadkashif3779@gmail.com>
2023-10-20 11:34:35 +0500

    change title of the app

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Quran App',
+      title: 'Quran App In Flutter',
       theme: ThemeData(
 
         primarySwatch: Constants.mySwatchColor,
